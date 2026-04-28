06631c5708ba5618a86d675bbc30d0792bc1088a
resortsbd-rgb <resortsbd@gmail.com>
2026-04-27 21:45:23 -0500

    fix: change IIFE syntax from !function to (function) for TypeScript compatibility

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -34,18 +34,18 @@ export default function Analytics() {
         id="meta-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: 
-            !function(f,b,e,v,n,t,s)
+          __html: `
+            (function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
             n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            s.parentNode.insertBefore(t,s)})(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '');
+            fbq('init', '${META_PIXEL_ID}');
             fbq('track', 'PageView');
-          ,
+          `,
         }}
       />
       <noscript>
@@ -53,7 +53,7 @@ export default function Analytics() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src={https://www.facebook.com/tr?id=&ev=PageView&noscript=1}
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
           alt=""
         />
       </noscript>
@@ -63,18 +63,18 @@ export default function Analytics() {
         id="gtm"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: 
+          __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','');
-          ,
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
         }}
       />
       <noscript>
         <iframe
-          src={https://www.googletagmanager.com/ns.html?id=}
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
           height="0"
           width="0"
           style={{ display: 'none', visibility: 'hidden' }}
